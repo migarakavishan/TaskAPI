e8527860eab70b0361b07be375f88b9ce1a7c857
migarakavishan <migarakavishan43@gmail.com>
2025-01-12 15:33:25 +0530

    Add Todo feature, remove TasksController, update to .NET 8.0
    Removed TasksController.cs and updated TaskAPI.csproj to .NET 8.0.
    Enabled nullable reference types and implicit usings.
    
    Added new Todo feature:
    - TodosController.cs: Handles CRUD operations for Todo items using TodoService.
    - Todo.cs: Defines the Todo class with properties Id, Title, Description, CreatedDate, Due, and Status.
    - TodoStatus.cs: Defines the TodoStatus enumeration with values New, Inprogress, and Completed.
    - TodoService.cs: Provides a method AllTodos to return a list of predefined Todo items.

---
+++ TaskAPI/Controllers/TodosController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskAPI.Models;
+using TaskAPI.Services;
+
+namespace TaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private TodoService _todoService;
+        public TasksController()
+        {
+            _todoService = new TodoService();
+        }
+
+        [HttpGet("{id?}")]
+        public IActionResult GetTodos(int? id)
+        {
+            var myTodos = _todoService.AllTodos();
+
+            if (id is null) return Ok(myTodos);
+
+            myTodos = myTodos.Where(t => t.Id == id).ToList();
+
+            return Ok(myTodos);
+        }
+
+        
+    }
+}
+ 
---
+++ TaskAPI/Services/TodoService.cs
@@ -0,0 +1,46 @@
+﻿using TaskAPI.Models;
+
+namespace TaskAPI.Services
+{
+    public class TodoService
+    {
+        public List<Todo> AllTodos()
+        {
+            var todos = new List<Todo>();
+            var todo1 = new Todo
+            {
+                Id = 1,
+                Title = "Get books for school",
+                Description = "Get some text books for school",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo1);
+
+            var todo2 = new Todo
+            {
+                Id = 2,
+                Title = "Get Vegitables",
+                Description = "Getvegitables for home",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(2),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo2);
+
+            var todo3 = new Todo
+            {
+                Id = 3,
+                Title = "Water the plants",
+                Description = "Water all plants ",
+                CreatedDate = DateTime.Now,
+                Due = DateTime.Now.AddDays(1),
+                Status = TodoStatus.New
+            };
+            todos.Add(todo3);
+
+            return todos;
+        }
+    }
+}
